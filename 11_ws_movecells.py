184af858064b818d12782b03aa49254a98361bea
neltia <dsz08082@naver.com>
2022-05-24 19:24:18 +0900

    # 9. 데이터 잘라내 붙이기

---
+++ 11_ws_movecells.py
@@ -0,0 +1,18 @@
+# 11_ws_movecells.py
+# - Post 9. 데이터 잘라내 붙이기
+
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment
+
+# 5_functions.py 파일의 결과 파일 경로 입력 후 파일 load
+path = "./preview_resultfile"
+wb = load_workbook(f'{path}/wb_multiple_func.xlsx')
+ws = wb['Sheet']
+
+ws.move_range("A2:F15", rows=3, cols=1)
+wb.save('wb_moving.xlsx')
+
+wb = load_workbook(f'{path}/wb_multiple_func.xlsx')
+ws = wb['Sheet']
+ws.move_range("A2:F15", rows=3, cols=1, translate=True)
+wb.save('wb_moving2.xlsx')
